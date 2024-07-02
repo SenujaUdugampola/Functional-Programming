71dce229d573593b26b61f5397d87d41fe7bc37f
SenujaUdugampola <110048598+SenujaUdugampola@users.noreply.github.com>
2024-07-02 07:00:15 +0530

    Update q3.scala

--- Practical-3/q3.scala
+++ Practical-3/q3.scala
@@ -1,26 +1,10 @@
 import scala.math.BigDecimal.RoundingMode
 
-def msum(num1: Int, num2: Int): Int = {
-    def msumrec(a: Int, b: Int, x: Int): Int ={
-        if(b == 0) x
-        else msumrec(a,b-1,x+1)
-    }
-    msumrec(num1, num2, 0)
+def average(a: Int, b: Int): Float = {
+    var average: Float = a.toFloat + b.toFloat
+    return BigDecimal(average/2).setScale(2, BigDecimal.RoundingMode.HALF_UP).toFloat
 }
 
-def amean(num1: Int, num2: Int): Unit = {
-    val tSum = msum(num1, num2)
-    val mean = tSum / 2.0
-}
-
-@main def calThings(): Float = {
-    println("Enter number 1 : ")
-    val num1 = scala.io.StdIn.readInt()
-
-    println("Enter number 2 : ")
-    val num2 = scala.io.StdIn.readInt()
-
-    val result = amean(num1,num2)
-    println(f"The arithmetic mean (average) of the two numbers $num1%d, $num2%d rounded to two decimal places is - $result%.2f")
-
+@main def run(): Unit = {
+    println("The arithmetic mean of 10 and 23, rounded to 2 decimal places is : "+average(10,23))
 }
