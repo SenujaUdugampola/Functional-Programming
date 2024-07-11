78c74db50d09fc84e4b4d6a2e475bdebb4c4d26d
SenujaUdugampola <110048598+SenujaUdugampola@users.noreply.github.com>
2024-07-11 18:48:08 +0530

    Update Q3.scala

--- Practical - 4/Q3.scala
+++ Practical - 4/Q3.scala
@@ -1,9 +1,19 @@
 def toUpper(str: String): String = {
-    str.toUpperCase()
+    str.map { char => 
+        if char >= 'a' && char <= 'z' then
+            (char - 32).toChar    
+        else
+            char
+    }
 }
 
 def toLower(str: String): String = {
-    str.toLowerCase()
+    str.map { char =>
+        if char >= 'A' && char <= 'Z' then
+            (char + 32).toChar
+        else
+            char    
+    }
 }
 
 def formatNames(name: String)(formatFunc: String => String): String = {
@@ -15,16 +25,16 @@ def formatNames(name: String)(formatFunc: String => String): String = {
 
     println(formatNames(names(0))(toUpper))
     println(formatNames(names(1)) { name =>
-        val firstPart = name.substring(0,2).toUpperCase
-        val secondPart = name.substring(2)
+        val firstPart = toUpper(name.substring(0,2))
+        val secondPart = toLower(name.substring(2))
         firstPart + secondPart
     })
 
     println(formatNames(names(2))(toLower))
     println(formatNames(names(3)) { name => 
-        val firstPart = name.substring(0, 1).toUpperCase()
-        val secondPart = name.substring(1, 5).toLowerCase()
-        val lastPart = name.substring(5).toUpperCase()
+        val firstPart = toUpper(name.substring(0, 1))
+        val secondPart = toLower(name.substring(1, 5))
+        val lastPart = toUpper(name.substring(5))
         firstPart + secondPart + lastPart
     })
 }
