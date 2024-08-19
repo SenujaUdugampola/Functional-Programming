b06326f5364a36dd9069382ae1b2da28c56bae11
SenujaUdugampola <110048598+SenujaUdugampola@users.noreply.github.com>
2024-08-20 03:55:54 +0530

    Update q2.scala

--- Practical - 9/q2.scala
+++ Practical - 9/q2.scala
@@ -3,7 +3,7 @@
     val Number:Int => String = {
         case x if x == 0 => "Zero"
         case x if x % 2 == 0 => "Even Number"
-        case x if x % 2 == 1 => "Odd Numbe"
+        case x if x % 2 == 1 => "Odd Number"
         case _  => "Neagative"
     }
 
